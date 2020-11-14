89dbe31a97e5b19ec26eafe31f7cd2adf5fa77cf
andrean10 <andreanramadhan10@gmail.com>
2020-11-15 00:01:38 +0700

    Membuat File Controller

--- controller.js
+++ controller.js
@@ -1,1 +1,8 @@
-'use strict'
+'use strict';
+
+var response = require('./res');
+var connection = require('./connection');
+
+exports.index = function (req, res) {
+    response.ok("Aplikasi REST API berjalan!");
+}

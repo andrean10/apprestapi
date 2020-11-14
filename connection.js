74f363d32e93f48b596e241a08aa231db457f47e
andrean10 <andreanramadhan10@gmail.com>
2020-11-14 23:49:43 +0700

    Menambahkan file koneksi

---
+++ connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+// buat koneksi database
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dbrestapi"
+});
+
+conn.connect((err) => {
+    if (err) {
+        console.log(`Error connection: ${err.stack}`);
+        return;
+    }
+    console.log('succes');
+});
+
+module.exports = conn;
